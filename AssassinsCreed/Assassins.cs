8b8d4819f84a179a6bc8eb8486a29d516db55c72
Minaz Inamdar <i2inamdarminaz@gmail.com>
2021-03-18 22:54:14 +0530

    CRUD in ADO.net

---
+++ AssassinsCreed/Assassins.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AssassinsCreed
+{
+    public class Assassins
+    {
+        static string connectionStr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        SqlConnection con = new SqlConnection(connectionStr);
+        public void RetrieveAllAssassins()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select * from assassins", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                Console.WriteLine("AssassinID \tAssassin Name \t\t Profile\t\tSeries Name");
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t\t{1}\t\t\t{2}\t\t{3}", reader[0], reader[1], reader[2], reader[3]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void AddCharacter(string name,string profile,string series)
+        {
+           try
+            {
+                SqlCommand cmd = new SqlCommand("insert into assassins values (@assassinName,@profile,@series)", con);
+                cmd.Parameters.AddWithValue("@assassinName", name);
+                cmd.Parameters.AddWithValue("@profile", profile);
+                cmd.Parameters.AddWithValue("@series", series);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                    Console.WriteLine("Record Successfully inserted");
+                else
+                    Console.WriteLine("Failed to insert the assassin.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void DeleteCharacter(int id)
+        {
+            try {
+                SqlCommand cmd = new SqlCommand("delete from assassins where assassinID=@assassinID", con);
+                cmd.Parameters.AddWithValue("@assassinID", id);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if(rowsAffected>0)
+                    Console.WriteLine("Record Successfully deleted");
+                else
+                    Console.WriteLine("Failed to delted the assassin. Check the Assassin ID");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }   
+        }
+
+        public void UpdateCharacter(string name,int id)
+        {
+          try
+            {
+                SqlCommand cmd = new SqlCommand("update assassins set assassinName= @assassinName where assassinID=@assassinID", con);
+                cmd.Parameters.AddWithValue("@assassinName", name);
+                cmd.Parameters.AddWithValue("@assassinID", id);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                    Console.WriteLine("Record Successfully updated");
+                else
+                    Console.WriteLine("Failed to update the assassin. Check the Assassn ID");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void SearchCharacter(string name)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select * from assassins where assassinName=@name", con);
+                cmd.Parameters.AddWithValue("@name", name);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                Console.WriteLine("AssassinID \tAssassin Name \t\t Profile\t\tSeries Name");
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t\t{1}\t\t\t{2}\t\t{3}", reader[0], reader[1], reader[2], reader[3]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void CountCharacters()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select count(assassinID) from assassins", con);
+                con.Open();
+                int count = (int)cmd.ExecuteScalar();
+                Console.WriteLine("There's {0} assassins in our Creeed",count); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void DisplayTemplars()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select * from assassins where profile='Templar' or profile='Chinese Templar'", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    Console.WriteLine("{0}\t\t{1}\t\t\t{2}\t\t{3}", reader[0], reader[1], reader[2], reader[3]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ AssassinsCreed/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AssassinsCreed
+{
+    class Program
+    {
+        static void Main()
+        {
+            Assassins a = new Assassins();
+            char ans;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("ASSASSIN's CREED\n");
+                    Console.WriteLine("1.Display all Characters");
+                    Console.WriteLine("2.Add a new Character");
+                    Console.WriteLine("3.Delete a Character");
+                    Console.WriteLine("4.Update a Character");
+                    Console.WriteLine("5.Search a Character");
+                    Console.WriteLine("6. Number of Assassins in the creed");
+                    Console.WriteLine("7.Display  all Templars\n");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            a.RetrieveAllAssassins();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter the Assassin name");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Enter the Profile name");
+                            string profile = Console.ReadLine();
+                            Console.WriteLine("Enter the Series name");
+                            string series = Console.ReadLine();
+                            a.AddCharacter(name, profile, series);
+                            break;
+                        case 3:
+                            a.RetrieveAllAssassins();
+                            Console.WriteLine("Enter the Assassin ID to Delete");
+                            int id = Convert.ToInt32(Console.ReadLine());
+                            a.DeleteCharacter(id);
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter the new Assassin name");
+                            string newname = Console.ReadLine();
+                            Console.WriteLine("Enter the Assassin ID");
+                            int id1 = Convert.ToInt32(Console.ReadLine());
+                            a.UpdateCharacter(newname, id1);
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter the profile you want to search");
+                            string searchProfile = Console.ReadLine();
+                            a.SearchCharacter(searchProfile); 
+                            break;
+                        case 6:
+                            a.CountCharacters();
+                            break;
+                        case 7:
+                            a.DisplayTemplars();
+                            break;
+                    }
+                    Console.WriteLine("\nDo you want to continue? y/n");
+                    ans = Console.ReadLine()[0];
+                } while (ans != 'n');
+                }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message, exception.StackTrace);
+            }
+
+        }
+    }
+}
